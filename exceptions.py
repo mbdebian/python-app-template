0bf3b2b2aae2cb647f73470d5797e5982d2d0da5
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-07-15 09:27:15 +0100

    exceptions module placeholder

---
+++ exceptions.py
@@ -0,0 +1,12 @@
+# 
+# Author    : Manuel Bernal Llinares
+# Project   : python-app-template
+# Timestamp : 15-07-2017 9:27
+# ---
+# © 2017 Manuel Bernal Llinares <mbdebian@gmail.com>
+# All rights reserved.
+# 
+
+"""
+Document your module here
+"""
