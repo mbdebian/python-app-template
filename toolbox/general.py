9bc001a87dfb53a16642c2cadbd84e9a474bcb9b
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-07-16 16:47:23 +0100

    read json from file, documentation

--- toolbox/general.py
+++ toolbox/general.py
@@ -15,5 +15,12 @@ import json
 
 
 def read_json(json_file="json_file_not_specified.json"):
+    """
+    Reads a json file and it returns its object representation, no extra checks
+    are performed on the file so, in case anything happens, the exception will
+    reach the caller
+    :param json_file: path to the file in json format to read
+    :return: an object representation of the data in the json file
+    """
     with open(json_file) as jf:
         return json.load(jf)
