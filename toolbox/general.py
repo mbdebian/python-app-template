be142a7bc575f7907735f999a4a2c27d61d75dd5
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-07-16 16:44:46 +0100

    general toolbox

---
+++ toolbox/general.py
@@ -0,0 +1,12 @@
+# 
+# Author    : Manuel Bernal Llinares
+# Project   : python-app-template
+# Timestamp : 16-07-2017 16:44
+# ---
+# © 2017 Manuel Bernal Llinares <mbdebian@gmail.com>
+# All rights reserved.
+# 
+
+"""
+Document your module here
+"""
