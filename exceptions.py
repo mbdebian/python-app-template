a2b0a54ff8aba26cad7ddae38c289e5a8c35e606
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-07-16 16:35:30 +0100

    configuration exception

--- exceptions.py
+++ exceptions.py
@@ -18,3 +18,8 @@ class AppException(Exception):
 
     def __str__(self):
         return repr(self.value)
+
+
+class AppConfigException(AppException):
+    def __init__(self, value):
+        super(AppConfigException, self).__init__(value)
