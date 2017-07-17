33dca0f2ddefffd13a2add981510f293c872c965
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-07-17 07:34:23 +0100

    ConfigManagerException

--- exceptions.py
+++ exceptions.py
@@ -23,3 +23,12 @@ class AppException(Exception):
 class AppConfigException(AppException):
     def __init__(self, value):
         super(AppConfigException, self).__init__(value)
+
+
+class ConfigManagerException(Exception):
+    def __init__(self, value):
+        Exception.__init__(self)
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
