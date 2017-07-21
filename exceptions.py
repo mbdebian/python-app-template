0f641012a07635aa41bd208bbd2940cc1c73df48
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-07-21 10:27:51 +0100

    prevent the script from running in standalone mode

--- exceptions.py
+++ exceptions.py
@@ -37,3 +37,7 @@ class ConfigManagerException(Exception):
 class ToolBoxException(AppException):
     def __init__(self, value):
         super(ToolBoxException, self).__init__(value)
+
+
+if __name__ == '__main__':
+    print("ERROR: This script is part of a application and it is not meant to be run in stand alone mode")
