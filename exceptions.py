da8c8a9fef8c16ee500f112bda449801cc208748
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-07-21 10:24:49 +0100

    toolbox exception

--- exceptions.py
+++ exceptions.py
@@ -32,3 +32,8 @@ class ConfigManagerException(Exception):
 
     def __str__(self):
         return repr(self.value)
+
+
+class ToolBoxException(AppException):
+    def __init__(self, value):
+        super(ToolBoxException, self).__init__(value)
