db4c80c94e05e8b283edace29d3159424726765c
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-07-22 07:35:11 +0100

    application bootstrap script

---
+++ main_app.py
@@ -0,0 +1,12 @@
+# 
+# Author    : Manuel Bernal Llinares
+# Project   : python-app-template
+# Timestamp : 22-07-2017 7:34
+# ---
+# © 2017 Manuel Bernal Llinares <mbdebian@gmail.com>
+# All rights reserved.
+# 
+
+"""
+Document your module here
+"""
