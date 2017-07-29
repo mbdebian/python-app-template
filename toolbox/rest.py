9c01ffaa5a8d2579a710c08ebd883081d060d1b0
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-07-29 17:28:03 +0100

    restful request toolbox

---
+++ toolbox/rest.py
@@ -0,0 +1,12 @@
+# 
+# Author    : Manuel Bernal Llinares
+# Project   : python-app-template
+# Timestamp : 29-07-2017 17:27
+# ---
+# © 2017 Manuel Bernal Llinares <mbdebian@gmail.com>
+# All rights reserved.
+# 
+
+"""
+Document your module here
+"""
