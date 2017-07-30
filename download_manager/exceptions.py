a974cfc3d62dbf38ceebad1e9b8c6d1a7738c10b
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-07-30 19:22:03 +0100

    prevent the script from running in stand alone mode

--- download_manager/exceptions.py
+++ download_manager/exceptions.py
@@ -22,3 +22,7 @@ class ManagerException(AppException):
 class AgentException(AppException):
     def __init__(self, value):
         super(AgentException, self).__init__(value)
+
+
+if __name__ == '__main__':
+    print("ERROR: This script is part of a application and it is not meant to be run in stand alone mode")
