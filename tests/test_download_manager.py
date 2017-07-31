aef1669154820bf3238ed457cea8f6ee1568f423
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-07-31 11:16:52 +0100

    download manager unit tests, test case, unit test using ensembl urls

--- tests/test_download_manager.py
+++ tests/test_download_manager.py
@@ -19,3 +19,13 @@ from download_manager.manager import Manager as DownloadManager
 
 class TestDownloadManager(unittest.TestCase):
     __logger = config_manager.get_app_config_manager().get_logger_for(__name__)
+
+    def test_success_on_sample_files_download(self):
+        urls = ['ftp://ftp.ensembl.org/pub/release-89/fasta/homo_sapiens/pep/Homo_sapiens.GRCh38.pep.all.fa.gz',
+                'ftp://ftp.ensembl.org/pub/release-89/fasta/homo_sapiens/pep/Homo_sapiens.GRCh38.pep.abinitio.fa.gz',
+                'ftp://ftp.ensembl.org/pub/release-89/gtf/homo_sapiens/Homo_sapiens.GRCh38.89.abinitio.gtf.gz',
+                'ftp://ftp.ensembl.org/pub/release-89/gtf/homo_sapiens/Homo_sapiens.GRCh38.89.chr.gtf.gz']
+
+
+if __name__ == '__main__':
+    print("ERROR: This script is part of a application and it is not meant to be run in stand alone mode")
