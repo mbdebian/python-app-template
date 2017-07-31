702e2bf3a1610d5fe56a0f926dadb0d80a71bf0f
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-07-31 11:12:47 +0100

    download manager unit tests placeholder

---
+++ tests/test_download_manager.py
@@ -0,0 +1,12 @@
+# 
+# Author    : Manuel Bernal Llinares
+# Project   : python-app-template
+# Timestamp : 31-07-2017 11:12
+# ---
+# © 2017 Manuel Bernal Llinares <mbdebian@gmail.com>
+# All rights reserved.
+# 
+
+"""
+Document your module here
+"""
