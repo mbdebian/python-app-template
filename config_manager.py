1ec87af11a7655ede743198aa50d1e4173dc0139
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-09-02 03:24:40 +0100

    get_folder_config, use absolute path

--- config_manager.py
+++ config_manager.py
@@ -177,7 +177,7 @@ class AppConfigManager(ConfigurationManager):
 
     def get_folder_config(self):
         # Configuration folder cannot be changed in this version of the template
-        return _folder_config
+        return os.path.abspath(_folder_config)
 
     def get_folder_logs(self):
         # Configuration for logging folder cannot be changed in this version of the template
