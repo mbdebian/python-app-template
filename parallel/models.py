2ac595e96282d90140199de35197ee9451400804
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-09-17 06:21:40 +0100

    parallel module models, placeholder

---
+++ parallel/models.py
@@ -0,0 +1,12 @@
+# 
+# Author    : Manuel Bernal Llinares
+# Project   : python-app-template
+# Timestamp : 17-09-2017 6:21
+# ---
+# © 2017 Manuel Bernal Llinares <mbdebian@gmail.com>
+# All rights reserved.
+# 
+
+"""
+Document your module here
+"""
