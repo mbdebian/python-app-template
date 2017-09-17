4bbf9abe8559325f2e5bc29a2939ef9ff9303583
Manuel Bernal Llinares <mbdebian@gmail.com>
2017-09-17 06:18:41 +0100

    parallel runner base exception

--- parallel/exceptions.py
+++ parallel/exceptions.py
@@ -24,3 +24,9 @@ class NoMoreAliveRunnersException(ParallelRunnerManagerException):
     def __init__(self, value):
         super().__init__(value)
 
+
+class ParallelRunnerException(AppException):
+    def __init__(self, value):
+        super().__init__(value)
+
+
